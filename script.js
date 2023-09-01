29ca149e809b0a7c6fdd66467c5eb6fc308b402d
aaryak4554 <joinaarya30@gmail.com>
2023-09-01 18:04:02 +0530

    added songs and art

--- script.js
+++ script.js
@@ -18,3 +18,32 @@ let index = 0;
 let isPlaying = false;
 let isRandom = false;
 let updateTimer;
+
+
+
+const songData = [
+    {
+        img : 'images/stay.png',
+        name : 'Stay',
+        artist : 'The Kid LAROI, Justin Bieber',
+        music : 'music/stay.mp3'
+    },
+    {
+        img : 'images/fallingdown.jpg',
+        name : 'Falling Down',
+        artist : 'Wid Cards',
+        music : 'music/fallingdown.mp3'
+    },
+    {
+        img : 'images/faded.png',
+        name : 'Faded',
+        artist : 'Alan Walker',
+        music : 'music/Faded.mp3'
+    },
+    {
+        img : 'images/ratherbe.jpg',
+        name : 'Rather Be',
+        artist : 'Clean Bandit',
+        music : 'music/Rather Be.mp3'
+    }
+];
